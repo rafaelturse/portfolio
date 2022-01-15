8264a66e22b165ff8698927baa7114369ae6b3fe
rafaelturse <rafaelturse@hotmail.com>
2022-01-15 15:29:57 -0500

    Updates

---
+++ precache-manifest.59009aafae44bfd0db0c497959a11d18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326680fc4dd0c0f701895773de01d14a",
+    "url": "rafaelturse.ca/index.html"
+  },
+  {
+    "revision": "95f38d2537c87ca6f258",
+    "url": "rafaelturse.ca/static/css/main.c95d7370.chunk.css"
+  },
+  {
+    "revision": "46a7bbc95475dd399ed8",
+    "url": "rafaelturse.ca/static/js/2.cb236cd9.chunk.js"
+  },
+  {
+    "revision": "95f38d2537c87ca6f258",
+    "url": "rafaelturse.ca/static/js/main.0a311f75.chunk.js"
+  },
+  {
+    "revision": "764cb35f76d3777fe1a6",
+    "url": "rafaelturse.ca/static/js/runtime~main.741f700a.js"
+  },
+  {
+    "revision": "9c6d8dc3c1cf5f654c653054d1181c27",
+    "url": "rafaelturse.ca/static/media/logo-rafaelturse-horizontal-dark.9c6d8dc3.svg"
+  },
+  {
+    "revision": "95d30e977b8e7a89825c5fdf9fdd8370",
+    "url": "rafaelturse.ca/static/media/logo-rafaelturse-horizontal-light.95d30e97.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db99de6b8c2ed0b5a5957a1faa54ddd5.js"
+  "rafaelturse.ca/precache-manifest.59009aafae44bfd0db0c497959a11d18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("rafaelturse.ca/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
