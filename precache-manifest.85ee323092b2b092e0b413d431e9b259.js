c8f5180468669649d79aa402a7555f398ba00885
rafaelturse <rafaelturse@hotmail.com>
2022-01-15 01:29:21 -0500

    Updates

---
+++ precache-manifest.85ee323092b2b092e0b413d431e9b259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c061b1dffa503d9129c9c05b88a4989f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f4da061336d10620f42c",
+    "url": "/portfolio/static/css/main.547b8fe3.chunk.css"
+  },
+  {
+    "revision": "78ea30c19d61f5f62986",
+    "url": "/portfolio/static/js/2.a9576760.chunk.js"
+  },
+  {
+    "revision": "f4da061336d10620f42c",
+    "url": "/portfolio/static/js/main.d8cb98ab.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "95d30e977b8e7a89825c5fdf9fdd8370",
+    "url": "/portfolio/static/media/logo-rafaelturse-horizontal-light.95d30e97.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rafaelturse/portfolio/precache-manifest.2d000e5fe7b3aee4cbf7507c5e053663.js"
+  "/portfolio/precache-manifest.85ee323092b2b092e0b413d431e9b259.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rafaelturse/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
