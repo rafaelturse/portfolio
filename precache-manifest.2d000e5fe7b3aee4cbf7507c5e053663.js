04c676797c2d47f6479aa1f15e2992a565903df2
rafaelturse <rafaelturse@hotmail.com>
2022-01-15 01:27:38 -0500

    Updates

---
+++ precache-manifest.2d000e5fe7b3aee4cbf7507c5e053663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd640ad9900f032f8b4cb3a115188797",
+    "url": "/rafaelturse/portfolio/index.html"
+  },
+  {
+    "revision": "f4da061336d10620f42c",
+    "url": "/rafaelturse/portfolio/static/css/main.547b8fe3.chunk.css"
+  },
+  {
+    "revision": "78ea30c19d61f5f62986",
+    "url": "/rafaelturse/portfolio/static/js/2.a9576760.chunk.js"
+  },
+  {
+    "revision": "f4da061336d10620f42c",
+    "url": "/rafaelturse/portfolio/static/js/main.d8cb98ab.chunk.js"
+  },
+  {
+    "revision": "9953b2dcdee01aabff78",
+    "url": "/rafaelturse/portfolio/static/js/runtime~main.144c10d5.js"
+  },
+  {
+    "revision": "95d30e977b8e7a89825c5fdf9fdd8370",
+    "url": "/rafaelturse/portfolio/static/media/logo-rafaelturse-horizontal-light.95d30e97.svg"
+  }
+]);
