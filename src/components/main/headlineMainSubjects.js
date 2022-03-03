9e688068c39b96d465ba1de265e241c82e79e1c5
rafaelturse <rafaelturse@hotmail.com>
2022-03-02 20:54:58 -0500

    icon updating

--- src/components/main/headlineMainSubjects.js
+++ src/components/main/headlineMainSubjects.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import LocalStorageService from '../../service/localStorageService';
+import LocalStorageService from '../../service/localStorageService'
+
+import {ReactComponent as IconAddressBook} from '../../art/icon/pack/address-book.svg'
+import {ReactComponent as IconTrophy} from '../../art/icon/pack/trophy.svg'
+import {ReactComponent as IconGear} from '../../art/icon/pack/gear.svg'
 
 class HeadlineMainSubjects extends React.Component {
 
@@ -8,32 +12,28 @@ class HeadlineMainSubjects extends React.Component {
     componentDidMount() { LocalStorageService.setItem('doClickComponent', '') }
 
     render() {
+        const faSecondary = {opacity:.4};
+
         return (
             <section>
                 <div className="row my-5">
                     <div className="col card-link text-center p-5">
                         <a href="#/technical" onClick={() => this.setDoClickComponent('resume')}>
-                            <svg className="bi bi-person-circle mb-4" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                            </svg>
+                            <IconAddressBook />
                             <div className='fs-2'>Resume</div>
                             <p>short description about my journey, education and career, ready to download</p>
                         </a>
                     </div>
                     <div className="col card-link text-center p-5">
                         <a href="#/professional">
-                            <svg className="bi bi-person-circle mb-4" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1z" />
-                            </svg>
+                            <IconTrophy />
                             <div className='fs-2'>Professional</div>
                             <p>contact information, education, career, profile and some curiosities about me</p>
                         </a>
                     </div>
                     <div className="col card-link text-center p-5">
-                        <a href="#/technical">
-                            <svg className="bi bi-person-circle mb-4" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
-                            </svg>
+                        <a href="#/technical">                            
+                            <IconGear />
                             <div className='fs-2'>Technical</div>
                             <p>information about development skills and programming languages</p>
                         </a>
