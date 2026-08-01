0745b54ef872328b14dd566e0124b906e48f588b
Rafael Turse <rafaelturse@hotmail.com>
2026-08-01 09:27:15 -0400

    helloworld

--- src/components/professional/educationTableSubject.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function EducationTableSubject({ ...props }) {
-    return (
-        <tr className="table-subject"><td colspan="2"><span className="text-skill text-center"><div className="fs-5">{props.title}</div></span></td></tr>
-    )
-}
-
-export default EducationTableSubject
--- src/components/technical/landscapeItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LandscapeItem({ ...props }) {
-    return (
-        <div className="img-fluid image-zoon-icon d-flex justify-content-center m-2"><img className="image-main-icon" src={props.path} alt="..." /></div>
-    )
-}
-
-export default LandscapeItem
